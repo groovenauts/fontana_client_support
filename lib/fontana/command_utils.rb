6b32b48ca7d13e0256729449f5927fb1ae6d8669
akima <akm2000@gmail.com>
2013-07-12 18:12:43 +0900

    [#53279001] 子プロセスの実行結果がエラーだった場合に、エラーとして出力するようにしました。

--- lib/fontana/command_utils.rb
+++ lib/fontana/command_utils.rb
@@ -8,13 +8,16 @@ module Fontana
     def system!(cmd)
       puts "now executing: #{cmd}"
 
+      buf = []
       IO.popen("#{cmd} 2>&1") do |io|
         while line = io.gets
           puts line
+          buf << line
         end
       end
 
       if $?.exitstatus != 0
+        $stderr.puts("\e[31mFAILURE: %s\n%s\e[0m" % [cmd, buf.join.strip])
         exit(1)
       end
     end
