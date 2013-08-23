14590950095cd4f22ff425020be02ab542d783b5
akima <akm2000@gmail.com>
2013-08-23 14:56:32 +0900

    spawn_at_vendor_fontanaをspawn_at_vendor_fontana_with_sweeperを分けました

--- lib/fontana/command_utils.rb
+++ lib/fontana/command_utils.rb
@@ -13,17 +13,23 @@ module Fontana
       end
     end
 
-    def spawn_at_vendor_fontana(env, cmd)
-      options = { chdir: FontanaClientSupport.vendor_fontana, out: "/dev/null" }
+    def spawn_at_vendor_fontana(env, cmd, options = {})
+      options = { chdir: FontanaClientSupport.vendor_fontana }.update(options)
       env = env.each_with_object({}){|(k,v), d| d[k.to_s] = v.to_s }
       puts "now spawning:\n  env: #{env.inspect}\n  cmd: #{cmd.inspect}\n  options: #{options.inspect}"
       pid = spawn(env, cmd, options)
       puts "spawning suceeded pid: #{pid.inspect}"
       Process.detach(pid)
+      return pid
+    end
+
+    def spawn_at_vendor_fontana_with_sweeper(env, cmd)
+      pid = spawn_at_vendor_fontana(env, cmd)
       at_exit{
         puts "Now killing #{pid}"
         Process.kill("INT", pid)
       }
+      pid
     end
 
     def system!(cmd)
